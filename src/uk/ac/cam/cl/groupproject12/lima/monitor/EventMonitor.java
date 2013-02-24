85443d596b09c1f492808055e3c07d9c2c3e1297
Matthew Huxtable <mjh233@cam.ac.uk>
2013-02-24 23:36:24 +0000

    Initial code for the Event Monitor.	This implements lockfile functionality in the core daemon (EventMonitor) using a file on disk to prevent multiple instances being started on any individual system.

---
+++ src/uk/ac/cam/cl/groupproject12/lima/monitor/EventMonitor.java
@@ -0,0 +1,101 @@
+package uk.ac.cam.cl.groupproject12.lima.monitor;
+
+import java.io.*;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+/**
+ * Entry point for starting an instance of the Event Monitor daemon. Configures
+ * the environment accordingly.
+ * 
+ * @author Team Lima
+ */
+public class EventMonitor
+{
+  private FileLock filelock;
+
+  /**
+   * Constructor for an instance of an EventMonitor.
+   */
+  EventMonitor()
+  {
+    // Enforce a single instance of the Monitor daemon.
+    try
+    {
+      singletonInstance();
+    } catch (SingleInstanceException e)
+    {
+      System.err.println(String
+          .format(Constants.DAEMON_ERROR_LOCKFILE_UNABLE_TO_ACQUIRE_LOCK));
+      System.exit(1);
+    }
+
+    // For testing multiple instances and the lockfile.
+    try
+    {
+      Thread.sleep(100000);
+    } catch (InterruptedException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Enforces the requirement that only one instance of the daemon should run at
+   * any time on the system.
+   * 
+   * @throws SingleInstanceException
+   * 
+   * @throws IOException
+   */
+  private void singletonInstance() throws SingleInstanceException
+  {
+    String location = Constants.DAEMON_LOCKFILE_NAME_DEFAULT;
+    File lockfile = new File(location);
+
+    // Attempt to open and obtain a lock on the lockfile's channel
+    try
+    {
+      // Create the file, if it does not already exist.
+      lockfile.createNewFile();
+      FileOutputStream out = new FileOutputStream(lockfile);
+      FileChannel channel = out.getChannel();
+      this.filelock = channel.tryLock();
+
+      if (this.filelock == null)
+      {
+        throw new SingleInstanceException(location);
+      }
+    } catch (IOException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void finalize()
+  {
+    // Release the file lock
+    try
+    {
+      this.filelock.release();
+    } catch (IOException e)
+    {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Entry point for launching an instance of the EventMonitor daemon.
+   * 
+   * @param args
+   * @throws SingleInstanceException
+   */
+  public static void main(String[] args) throws SingleInstanceException
+  {
+    EventMonitor e = new EventMonitor();
+  }
+}
