e349b285a7b18ba12a05c3c095a6d2d9aabf897c
Matthew Huxtable <mjh233@cam.ac.uk>
2013-02-25 21:54:42 +0000

    Removed unneeded constants.

--- src/uk/ac/cam/cl/groupproject12/lima/monitor/Constants.java
+++ src/uk/ac/cam/cl/groupproject12/lima/monitor/Constants.java
@@ -6,9 +6,6 @@ package uk.ac.cam.cl.groupproject12.lima.monitor;
  * @author Team Lima
  */
 public class Constants {
-    public static final String DAEMON_ERROR_LOCKFILE_NON_FILE = "The lockfile at location %s is not a valid file but rather has some other file system object type.";
-    public static final String DAEMON_ERROR_LOCKFILE_UNABLE_TO_ACQUIRE_LOCK = "Unable to acquire a lock on the lockfile. Another instance of this daemon may be running, thereby preventing another from starting.";
-    public static final String DAEMON_LOCKFILE_NAME_DEFAULT = ".monitorLockfile";
     public static final String HBASE_CONFIGURATION_ZOOKEEPER_QUORUM = "hbase.zookeeper.quorum";
     public static final String HBASE_CONFIGURATION_ZOOKEEPER_CLIENTPORT = "hbase.zookeeper.property.clientPort";
 }
