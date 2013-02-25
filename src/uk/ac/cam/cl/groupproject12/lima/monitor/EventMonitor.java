dfb2aabf3cdc417670fcf4757dfadd84443836db
Matthew Huxtable <mjh233@cam.ac.uk>
2013-02-25 21:54:33 +0000

    New EventMonitor in accordance with the new "paradigm".

---
+++ src/uk/ac/cam/cl/groupproject12/lima/monitor/EventMonitor.java
@@ -0,0 +1,12 @@
+package uk.ac.cam.cl.groupproject12.lima.monitor;
+
+/**
+ * Manages the replication of data between HBase and PostgreSQL on completion of
+ * a Hadoop M-R job.
+ * 
+ * @author Team Lima
+ * 
+ */
+public class EventMonitor {
+
+}
