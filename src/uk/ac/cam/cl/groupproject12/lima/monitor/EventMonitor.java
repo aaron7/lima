8d16c1ca8b05e9ee89402fd0ee444ea98e00fe04
Matthew Huxtable <mjh233@cam.ac.uk>
2013-02-25 22:29:06 +0000

    HBaseConfiguration in Monitor.

--- src/uk/ac/cam/cl/groupproject12/lima/monitor/EventMonitor.java
+++ src/uk/ac/cam/cl/groupproject12/lima/monitor/EventMonitor.java
@@ -1,5 +1,8 @@
 package uk.ac.cam.cl.groupproject12.lima.monitor;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+
 /**
  * Manages the replication of data between HBase and PostgreSQL on completion of
  * a Hadoop M-R job.
@@ -8,5 +11,15 @@ package uk.ac.cam.cl.groupproject12.lima.monitor;
  * 
  */
 public class EventMonitor {
+
+	Configuration hbaseConfig = HBaseConfiguration.create();
+
+	public EventMonitor() {
+		
+		hbaseConfig.set(Constants.HBASE_CONFIGURATION_ZOOKEEPER_QUORUM,
+				"localhost");
+		hbaseConfig.setInt(Constants.HBASE_CONFIGURATION_ZOOKEEPER_CLIENTPORT,
+				2182);
+	}
 
 }
