1d2f6fc438cf24e2bf6a0dd4b12b9e6fed32c35c
Matthew Huxtable <mjh233@cam.ac.uk>
2013-02-25 00:54:23 +0000

    JavaDoc on ThreatThread.java and StatisticThread.java to describe their high-level functionality.

--- src/uk/ac/cam/cl/groupproject12/lima/monitor/StatisticThread.java
+++ src/uk/ac/cam/cl/groupproject12/lima/monitor/StatisticThread.java
@@ -1,5 +1,11 @@
 package uk.ac.cam.cl.groupproject12.lima.monitor;
 
-
+/**
+ * This class encapsulates the actions required to read in statistics from HBase,
+ * aggregate them appropriately and write them out to the PostgreSQL relational
+ * database for data visualisation.
+ *
+ * @author Team Lima
+ */
 public class StatisticThread {
 }
--- src/uk/ac/cam/cl/groupproject12/lima/monitor/ThreatThread.java
+++ src/uk/ac/cam/cl/groupproject12/lima/monitor/ThreatThread.java
@@ -1,4 +1,10 @@
 package uk.ac.cam.cl.groupproject12.lima.monitor;
 
+/**
+ * This class encapsulates the actions required to read in threats from HBase
+ * and write them out to the PostgreSQL relational database for data visualisation.
+ *
+ * @author Team Lima
+ */
 public class ThreatThread {
 }
