ad30f4ebb6c670c1b13704407588b3bcc5a94b0c
Matthew Huxtable <mjh233@cam.ac.uk>
2013-02-26 12:52:15 +0000

    Added ENUMs for EventStatus which should tally with the status column in Postgres

--- src/uk/ac/cam/cl/groupproject12/lima/monitor/EventStatus.java
+++ src/uk/ac/cam/cl/groupproject12/lima/monitor/EventStatus.java
@@ -2,6 +2,16 @@ package uk.ac.cam.cl.groupproject12.lima.monitor;
 
 public enum EventStatus {
 
-    //TODO
+	EVENT_OPEN(1), EVENT_RESOLVED(2);
+
+	private final int eventID;
+
+	EventStatus(int eventID) {
+		this.eventID = eventID;
+	}
+
+	public int getEventID() {
+		return this.eventID;
+	}
 
 }
