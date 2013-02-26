fe500fe6d11a72de93a7fad6f966144c8bcc149d
Matthew Huxtable <mjh233@cam.ac.uk>
2013-02-26 00:09:23 +0000

    Updates to support filtering by router ID.

--- src/uk/ac/cam/cl/groupproject12/lima/monitor/ThreatSynchroniser.java
+++ src/uk/ac/cam/cl/groupproject12/lima/monitor/ThreatSynchroniser.java
@@ -1,11 +1,43 @@
 package uk.ac.cam.cl.groupproject12.lima.monitor;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.hadoop.hbase.filter.Filter;
+
 public class ThreatSynchroniser implements IDataSynchroniser {
+	private int routerID;
+	
+	/**
+	 * Constructs an instance of a threat synchroniser.
+	 * 
+	 * @param routerID
+	 *            The router ID we are to synchronise threats for.
+	 */
+	public ThreatSynchroniser(int routerID) {
+		this.routerID = routerID;
+	}
 
 	@Override
 	public boolean synchroniseTables(EventMonitor monitor) {
-		// TODO Auto-generated method stub
+
+		try {
+			HTable table = new HTable(monitor.getHbaseConfig(), "Threat");
+
+			// Set up a RowFilter to filter based on router ID
+			List<Filter> filters = new ArrayList<Filter>();
+			
+			Filter routerIDFilter = new SingleColumnValueFilter()
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		return false;
 	}
-
 }
