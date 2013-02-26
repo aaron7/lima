07e6c65e168cc0dd696b878835ffd49a96041ae9
Matthew Huxtable <mjh233@cam.ac.uk>
2013-02-26 01:47:11 +0000

    PGSQL_CONNECTION_XML_LOCATION should have a / to separate working directory from file name.

--- src/uk/ac/cam/cl/groupproject12/lima/monitor/Constants.java
+++ src/uk/ac/cam/cl/groupproject12/lima/monitor/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 	public static final String ERROR_POSTGRESQL_CONFIG_MALFORMED = "Please ensure all fields required are within the <pgsql> declaration.";
 
 	public static final String PGSQL_CONNECTION_STRING = "jdbc:postgresql://%s:%i/%s";
-	public static final String PGSQL_CONNECTION_XML_LOCATION = "%s.pgsqlConnectionConfig";
+	public static final String PGSQL_CONNECTION_XML_LOCATION = "%s/.pgsqlConnectionConfig";
 
 	public static final String HBASE_CONFIGURATION_ZOOKEEPER_QUORUM = "hbase.zookeeper.quorum";
 	public static final String HBASE_CONFIGURATION_ZOOKEEPER_CLIENTPORT = "hbase.zookeeper.property.clientPort";
