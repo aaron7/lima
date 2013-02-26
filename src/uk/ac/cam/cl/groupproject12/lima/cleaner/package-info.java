aa99c5096eb9c42244d44f6a7a98a9372c2e1e3d
Simon Hollingshead <sh690@cam.ac.uk>
2013-02-26 14:08:00 +0000

    Since it worked, I'll give all packages initial Javadoc.

---
+++ src/uk/ac/cam/cl/groupproject12/lima/cleaner/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package invoked to clean extraneous data from HBase tables based on a time limit.
+ */
+
+package uk.ac.cam.cl.groupproject12.lima.cleaner;
--- src/uk/ac/cam/cl/groupproject12/lima/monitor/package-info.java
+++ src/uk/ac/cam/cl/groupproject12/lima/monitor/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Module used to transfer data between HBase storage and the PostgreSQL database for the web UI.
+ * Package used to transfer data between HBase storage and the PostgreSQL database for the web UI.
  */
 
 package uk.ac.cam.cl.groupproject12.lima.monitor;
