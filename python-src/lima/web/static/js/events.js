d89a55fabae5f06d49e1551755b23ffa29e1bf2b
Aaron7 <ak759@cam.ac.uk>
2013-03-05 00:36:24 +0000

    last update before screenshots

--- python-src/lima/web/static/js/events.js
+++ python-src/lima/web/static/js/events.js
@@ -18,16 +18,75 @@ $.getJSON("/get?id=events", function(data){
     { "sTitle": "Type", "sClass": "control" },
     { "sTitle": "Status", "sClass": "control"},
     { "sTitle": "Message", "sClass": "control"},
-    { "sTitle": "Start Time", "sClass": "control"},
-    { "sTitle": "End Time", "sClass": "control"},
-    { "sTitle": "Time Submitted", "sClass": "control"}
+    { "sTitle": "Start Time", "sClass": "control",
+      "mRender": function ( data, type, full ) {
+        var date = new Date(data);
+        return date.toLocaleString();
+      }},
+    { "sTitle": "End Time", "sClass": "control",
+      "mRender": function ( data, type, full ) {
+        var date = new Date(data);
+        return date.toLocaleString();
+      }},
+    { "sTitle": "Time Submitted", "sClass": "control",
+      "mRender": function ( data, type, full ) {
+        var date = new Date(data);
+        return date.toLocaleString();
+      }
+  }
     ]
   } );
 
   //update info
   $("#numOfEvents").text(data.length);
 
+
+  //format timestamps
+for (var i = 0; i < data.length; i++) {
+  var date = new Date(data[i][1]);
+  data[i][1] = date.toLocaleString();
+  var date = new Date(data[i][5]);
+  data[i][5] = date.toLocaleString();
+}
+
 });
+
+//pie chart
+var data = [
+    { label: "TCP flooding",  data: 1},
+    { label: "Land Attack",  data: 1},
+    { label: "UDP Flooding",  data: 1},
+    { label: "Fraggle Attack",  data: 1},
+    { label: "PingPong",  data: 1},
+    { label: "ICMP Flooding",  data: 1},
+    { label: "Port Scan",  data: 1},
+    { label: "DOS Attack",  data: 1}];
+
+  // A custom label formatter used by several of the plots
+
+  function labelFormatter(label, series) {
+    return "<div style='font-size:8pt; text-align:center; padding:2px; color:black;'>" + label + "<br/>" + Math.round(series.percent) + "%</div>";
+  }
+
+
+$.plot('#piechart', data, {
+    series: {
+        pie: {
+            show: true,
+            radius: 1,
+            label: {
+                show: true,
+                radius: 2/3,
+                formatter: labelFormatter,
+                threshold: 0.1
+            }
+        }
+    },
+    legend: {
+        show: false
+    }
+});
+
 
 /*
 This section creates the channel streams to the server
